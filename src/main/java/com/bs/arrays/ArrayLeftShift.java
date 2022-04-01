3ee8245b5b32693bb87583014cd3b92cf0a307ab
Nikhil Wadatkar <nikhil.wadatkar4693@gmail.com>
2022-04-01 21:47:30 +0530

    Refactored

--- src/main/java/com/bs/arrays/ArrayLeftShift.java
+++ src/main/java/com/bs/arrays/ArrayLeftShift.java
@@ -4,22 +4,29 @@ import java.util.Scanner;
 
 public class ArrayLeftShift {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("enter the string");
-		String s = "123456";
-		char[] arr=s.toCharArray();
-		int n = arr.length;
-		char temp=arr[0];
-		for (int i = 0; i < n-1; i++) {
-			arr[i]=arr[i+1];
-		}
-		//asignng first element to last element
-		arr[n-1]=temp;
-		
-		for (int i = 0; i < n; i++) {
-			System.out.print(arr[i]);
-		}
-		
-	}
+    public static void main(String[] args) {
+        int noOfElements = 3;
+        int[] arr = {12, 15, 45, 98, 65, 98, 65, 6};
+        int n = arr.length;
+        showArray (arr, "Old Array: ", n);
+        for (int j = 0; j < noOfElements; j++) {
+            int temp = arr[0];
+            for (int i = 0; i < n - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+            //asignng first element to last element
+            arr[n - 1] = temp;
+        }
+
+        System.out.println ();
+        showArray (arr, "New Array: ", n);
+
+    }
+
+    private static void showArray(int[] arr, String message, int length) {
+        System.out.println (message);
+        for (int i = 0; i < length; i++) {
+            System.out.print (arr[i] + " ");
+        }
+    }
 }
