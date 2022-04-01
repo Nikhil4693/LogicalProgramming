29205e26d33de97840f77e7f622d79802d9128af
Nikhil Wadatkar <nikhil.wadatkar4693@gmail.com>
2022-04-01 20:36:52 +0530

    added rom intelliJ

---
+++ src/main/java/com/bs/LogicalProgramming/App3.java
@@ -0,0 +1,13 @@
+package com.bs.LogicalProgramming;
+
+/**
+ * Hello world!
+ *
+ */
+public class App3
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Ankur!" );
+    }
+}
