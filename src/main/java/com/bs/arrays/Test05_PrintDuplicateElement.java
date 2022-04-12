1abb73645e7c26d0045e90676606363306a81578
Ankur <ankur.wadatkar@gmail.com>
2022-04-12 12:14:04 +0530

    added print duplicate program

--- src/main/java/com/bs/arrays/Test05_PrintDuplicateElement.java
+++ src/main/java/com/bs/arrays/Test05_PrintDuplicateElement.java
@@ -2,31 +2,29 @@ package com.bs.arrays;
 
 public class Test05_PrintDuplicateElement {
 	public static void main(String args[]) {
-//		// Initialize array
-//		int[] arr = new int[] { 1, 2, 3, 4, 2, 7, 8, 8, 3 };
-//		System.out.println("Duplicate elements in given array: ");
-//		// Searches for duplicate element
-//		for (int i = 0; i < arr.length; i++) {
-//			for (int j = i + 1; j < arr.length; j++) {
-//				if (arr[i] == arr[j])
-//					System.out.println(arr[j]);
-//			}
-//		}
-		String str = "Nikhil Wadatkar";
-		char[] arr = str.toCharArray();
+		String string="malayalama";
+		char[] arr=string.toCharArray();
+		
+		System.out.println("Duplicate elements in given array: ");
+		// Searches for duplicate element
 		for (int i = 0; i < arr.length; i++) {
+			int count=1;
+		//search for duplicate and make it as '0' and increase the count.
 			for (int j = i + 1; j < arr.length; j++) {
-				if (arr[i] == arr[j]) {
-					System.out.println(arr[j]);
-
-//					int k = j;
-//					while (k < arr.length) {
-//						arr[k] = arr[k + 1];
-//						k++;
-//					}
+				if (arr[i] == arr[j]&& arr[i]!=' ') {
+					arr[j] = '0';
+				count++;
 				}
 			}
+			
+			//if count is > 1 then print it
+			if(count>1 && arr[i]!='0')
+				System.out.println(arr[i]);
 		}
-
+		
+		//see the affected array
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
 	}
 }
